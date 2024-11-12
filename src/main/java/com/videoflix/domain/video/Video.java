895655061c00ca619cc91f9dcf2ddfaf459afef7
Luiz Guilherme Pessoa da Silva <lguibtw@gmail.com>
2024-11-12 09:41:35 -0300

    add update method to video entity

--- src/main/java/com/videoflix/domain/video/Video.java
+++ src/main/java/com/videoflix/domain/video/Video.java
@@ -1,5 +1,6 @@
 package com.videoflix.domain.video;
 
+import com.videoflix.domain.UpdateVideoDTO;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -21,9 +22,23 @@ public class Video {
     private String descricao;
     private String url;
 
-    public Video(@Valid CreateVideoDTO dados) {
-        this.titulo = dados.titulo();
-        this.descricao = dados.descricao();
-        this.url = dados.url();
+    public Video(CreateVideoDTO data) {
+        this.titulo = data.titulo();
+        this.descricao = data.descricao();
+        this.url = data.url();
+    }
+
+    public void update(UpdateVideoDTO data) {
+        if (data.titulo() != null) {
+            this.titulo = data.titulo();
+        }
+
+        if (data.descricao() != null) {
+            this.descricao = data.descricao();
+        }
+
+        if (data.url() != null) {
+            this.url = data.url();
+        }
     }
 }
