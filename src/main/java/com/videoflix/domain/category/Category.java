8ae7768a896cd6fa6beeaf9f86e5128103a77a9f
Luiz Guilherme Pessoa da Silva <lguibtw@gmail.com>
2024-11-12 09:54:34 -0300

    add category entity

---
+++ src/main/java/com/videoflix/domain/category/Category.java
@@ -0,0 +1,21 @@
+package com.videoflix.domain.category;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "categories")
+@Entity(name = "Category")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String color;
+}
