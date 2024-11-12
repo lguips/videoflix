1f367ee3b6cfcb63a518df1b8e9ac134475451b4
Luiz Guilherme Pessoa da Silva <lguibtw@gmail.com>
2024-11-12 09:32:59 -0300

    create project

---
+++ src/main/java/com/videoflix/VideoflixApplication.java
@@ -0,0 +1,13 @@
+package com.videoflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoflixApplication.class, args);
+	}
+
+}
