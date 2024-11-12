ddf9cd40bf3d21224969e97eaf2b9039fcff69f4
Luiz Guilherme Pessoa da Silva <lguibtw@gmail.com>
2024-11-12 09:33:13 -0300

    create video entity

---
+++ src/main/java/com/videoflix/domain/video/Video.java
@@ -0,0 +1,29 @@
+package com.videoflix.domain.video;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "videos")
+@Entity(name = "Video")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Video {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private String url;
+
+    public Video(@Valid CreateVideoDTO dados) {
+        this.titulo = dados.titulo();
+        this.descricao = dados.descricao();
+        this.url = dados.url();
+    }
+}
